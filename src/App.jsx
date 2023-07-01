b18d400797c6dd4cbe6c66317588ad71d0720ee2
황상윤 <s_infinite@naver.com>
2023-07-01 19:58:50 +0900

    init. React BrowserRouter 위치 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 // import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "@/components/Home/Home";
 // import Navbar from "@/components/Navbar/Navbar";
 
-function App() {
-  console.log();
+const App = () => {
   return (
-    <Routes>
-      {/* <Route element={<Navbar />}> */}
-      <Route path="/" element={<Home />} />
-      <Route path="/mouse_cursor" element={<AAA />} />
-      {/* </Route> */}
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route element={<Navbar />}> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/mouse_cursor" element={<AAA />} />
+        {/* </Route> */}
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 function AAA() {
   return <h1>Hello AAA</h1>;
