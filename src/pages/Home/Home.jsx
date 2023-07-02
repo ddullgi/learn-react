8d51859773c242d542594db932ac03c5bc2efe7c
황상윤 <s_infinite@naver.com>
2023-07-03 05:05:37 +0900

    minesweepr. 테이블, 지뢰 세팅 함수 작성

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,9 @@ const Home = () => {
       <NavLink to="/mouse_cursor" className="link">
         <h1>마우스 커서 조정</h1>
       </NavLink>
+      <NavLink to="/mine_sweeper" className="link">
+        <h1>지뢰 찾기</h1>
+      </NavLink>
     </>
   );
 };
